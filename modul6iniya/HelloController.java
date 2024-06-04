6a7c2f53618a1c54319a5b16ccaf96492b018425
pipincantik <145654411+pipincantik@users.noreply.github.com>
2024-06-04 09:59:32 +0800

    Add files via upload

---
+++ modul6iniya/HelloController.java
@@ -0,0 +1,14 @@
+package org.example.modul6iniya;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
